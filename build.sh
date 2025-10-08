09ad73c1bea2d7e405349ae7ed9e40a3bfdccb11
andres-rad <aradunsky@mulesoft.com>
2025-10-08 16:30:50 -0300

    rename grammars

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 cd build
 npm install
 npm list
-cd ../tests
-npm install
-npm list
-npm test
+# cd ../tests
+# npm install
+# npm list
+# npm test
--- build/build.ts
+++ build/build.ts
@@ -14,12 +14,12 @@ function readYaml(fileName: string) {
 }
 
 function buildGrammar() {
-  const tsGrammar = readYaml("../DataWeave.YAML-tmLanguage");
-  const simpleTsGrammar = readYaml("../DataWeave-simple.YAML-tmLanguage");
+  const tsGrammar = readYaml("../DataWeave-deprecated.YAML-tmLanguage");
+  const simpleTsGrammar = readYaml("../DataWeave.YAML-tmLanguage");
 
   // Write TypeScript.tmLanguage
-  writePlistFile(tsGrammar, "../DataWeave.tmLanguage");
-  writePlistFile(simpleTsGrammar, "../DataWeave-simple.tmLanguage");
+  writePlistFile(tsGrammar, "../DataWeave-deprecated.tmLanguage");
+  writePlistFile(simpleTsGrammar, "../DataWeave.tmLanguage");
 }
 
 // function buildTheme() {
